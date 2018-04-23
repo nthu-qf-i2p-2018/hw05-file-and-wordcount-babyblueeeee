4a7a8b14a925d6de913ed6628d736ec5ff510f81
babyblueeeee <angalos1223@gmail.com>
2018-04-23 11:42:12 +0800

    add existing file

--- hw5.py
+++ hw5.py
@@ -1,52 +1,40 @@
 # -*- coding: utf-8 -*-
-import ...
+import json
+import pickle
+import csv
+import string
 
 
 def main(filename):
-    # read file into lines
-    lines = ...
+   t=open(filename)
+   lines=t.readlines()
+   all_words = []
+   for line in lines:
+     words=line.split()
+     for ele in words:
+       ele = ele.strip(string.punctuation) 
+       if ele!='':
+         all_words.append(ele)
+   counter = {}
+   for ele in all_words:
+     if ele in counter:
+       counter[ele]=counter[ele]+1
+     if ele not in counter:
+       counter.update({ele:1})
+   counters=sorted(counter.items(), key=lambda x: x[1],reverse=True)
+   counters=dict(counters)
+   with open('wordcount.csv', 'w', newline='') as csv_file:
+     writer = csv.writer(csv_file,delimiter=",")
+     writer.writerow(['word']+['count'])
+     for ele in counters:
+       writer.writerow([ele]+[counters[ele]])
+   with open("wordcount.json",'w') as json_file:
+     json.dump(counters,json_file)
+   with open("wordcount.pkl","wb") as pkl_file:
+     pickle.dump(counters, pkl_file)
 
-    # declare a word list
-    all_words = []
-
-    # extract all words from lines
-    for line in lines:
-        # split a line of text into a list words
-        # "I have a dream." => ["I", "have", "a", "dream."]
-        words = ...
-
-        # check the format of words and append it to "all_words" list
-        for word in words:
-            # then, remove (strip) unwanted punctuations from every word
-            # "dream." => "dream"
-            word = ...
-            # check if word is not empty
-            if word:
-                # append the word to "all_words" list
-                all_words...
-
-    # compute word count from all_words
-    counter = ...
-
-    # dump to a csv file named "wordcount.csv":
-    # word,count
-    # a,12345
-    # I,23456
-    # ...
-    with open(...) as csv_file:
-        # create a csv writer from a file object (or descriptor)
-        writer = ...
-        # write table head
-        writer.writerow(['word', 'count'])
-        # write all (word, count) pair into the csv writer
-        writer.writerows(...)
-
-    # dump to a json file named "wordcount.json"
-    ...
-
-    # BONUS: dump to a pickle file named "wordcount.pkl"
-    # hint: dump the Counter object directly
-
+main("i_have_a_dream.txt")
 
 if __name__ == '__main__':
-    main("i_have_a_dream.txt")
+   main("i_have_a_dream.txt")
+
